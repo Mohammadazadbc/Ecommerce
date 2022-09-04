ee78a9f5043d1b6098825b1717f5fb3a29258626
mohammad azad <mohammadazad.bc@gmail.com>
2022-09-04 12:46:16 +0200

    navbar responsive

---
+++ app.js
@@ -0,0 +1,15 @@
+const bar = document.getElementById('bar')
+const navbar = document.getElementById('navbar')
+const closeBtn = document.getElementById('close')
+
+if(bar){
+    bar.addEventListener('click',()=>{
+        navbar.classList.add('active')
+    })
+}
+
+if(closeBtn){
+    closeBtn.addEventListener('click',()=>{
+        navbar.classList.remove('active')
+    })
+}
